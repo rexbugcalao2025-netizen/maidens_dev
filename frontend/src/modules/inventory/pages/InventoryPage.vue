bb3b762a0bc7e86d710f8ccd92ec2b0c9b78b9db
rex_bugcalao <rexbugcalao2025@gmail.com>
2026-02-07 19:45:02 +0800

    added filter to inventory page

--- frontend/src/modules/inventory/pages/InventoryPage.vue
+++ frontend/src/modules/inventory/pages/InventoryPage.vue
@@ -1,7 +1,7 @@
 <!-- src/modules/inventory/pages/InventoryPage.vue -->
 
 <script setup>
-    import { ref, onMounted } from 'vue';
+    import { ref, onMounted, computed } from 'vue';
     import InventoryTable from '../components/InventoryTable.vue';
     import AdjustStockModal from '../components/AdjustStockModal.vue';
     import ConsumeStockModal from '../components/ConsumeStockModal.vue';
@@ -12,6 +12,9 @@
 
     const showAdjust = ref(false);
     const showConsume = ref(false);
+
+    // 🔍 SEARCH
+  const search = ref('');
 
     async function loadInventory() {
     products.value = await fetchInventory();
@@ -27,18 +30,51 @@
     showConsume.value = true;
     }
 
+    // ✅ FILTERED VIEW
+    const filteredProducts = computed(() => {
+      
+      if (!search.value) {
+        return products.value;
+      }
+
+      const q = search.value.toLocaleLowerCase();
+
+      return products.value.filter(p =>
+        p.name?.toLocaleLowerCase().includes(q) ||
+        p.category_name?.toLocaleLowerCase().includes(q) ||
+        p.sub_category_name?.toLocaleLowerCase().includes(q)
+      );
+    });
+
     onMounted(loadInventory);
 </script>
 
 <template>
   <div class="container-fluid p-4">
-    <h3 class="mb-4">
-      <i class="bi bi-box-seam me-2"></i>
-      Inventory
-    </h3>
+
+    <div class="d-flex justify-content-between align-items-center mb-3">
+      <h3 class="mb-0">
+        <i class="bi bi-box-seam me-2"></i>
+        Inventory
+      </h3>  
+
+      <!-- 🔍 SEARCH BOX -->
+       <div class="input-group" style="max-width: 320px;">
+          <span class="input-group-text">
+            <i class="bi bi-search"></i>
+          </span>
+          <input 
+            type="text"
+            class="form-control"
+            placeholder="Search product, category"
+            v-model="search"
+          />
+
+       </div>
+    </div>  
 
     <InventoryTable
-      :products="products"
+      :products="filteredProducts"
       @adjust="openAdjust"
       @consume="openConsume"
     />
